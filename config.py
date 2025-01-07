08acd0f3ed1d4f4b80473b75c317ce30938b0d52
Matthew Bonnema <matthew.bonnema@gmail.com>
2025-01-07 09:47:40 -0800

    update config

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
-GCS_PROJECT = 'opera-one'
-GCS_BUCKET = 'opera-bucket-dswx'
-GCS_PREFIX = 'collections/repo_testing/'
-GEE_COLLECTION_PATH = 'projects/opera-one/assets/DSWX/repo_testing'
+# Google Cloud Parameters
+GCS_PROJECT = 'ecipes-global-lakes-1'
+GCS_BUCKET = 'dswx-hls'
+GCS_PREFIX = 'collections/texas_reservoirs/'
+GEE_COLLECTION_PATH = 'projects/ecipes-global-lakes-1/assets/dswx-hls/texas-reservoirs'
 
+# OPERA Product Parameters
 PRODUCT = 'DSWx-HLS'
 BAND = 'CONF'
 
+# Search Parameters for DAAC Data Access
 URL_LIST = 'search_results.txt'
-STARTDATE = "2024-01-01"
-ENDDATE = "2024-02-13"
-MINLON = -119.96
-MAXLON = -119.39
-MINLAT = 35.7
-MAXLAT = 36.3
+STARTDATE = "2023-01-01"
+ENDDATE = "2025-01-01"
+MINLON = -100.6
+MAXLON = -92.2
+MINLAT = 28.9
+MAXLAT = 34.9
